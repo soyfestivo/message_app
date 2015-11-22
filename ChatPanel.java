6753b5060f129ef41b9c20f4b6afdade99df7a2e
KarenByk <kbyko2@uic.edu>
2015-11-21 23:23:13 -0600

    minor changes to chatpanel

--- ChatPanel.java
+++ ChatPanel.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.awt.Graphics;
 import javax.swing.*;
 import java.awt.GridLayout;
@@ -6,8 +7,12 @@ import java.awt.FlowLayout;
 
 public class ChatPanel extends JApplet
 {
-   private JButton sendButton;   //button to send message
-   private JTextArea msgField;   //text area for user to enter message to send
+   private JButton sendButton;      //button to send message
+   private JTextArea msgField;      //text area for user to enter message to send
+	private ArrayList<User> users;   //array of Ussers
+   private int numUsers;            //number of users
+   private User me;                 //this user's information
+   private int numMsgs = 500;             //number of messages in a conversation
    
    
    public void init()
@@ -27,7 +32,7 @@ public class ChatPanel extends JApplet
       sendPan.setLayout(new BorderLayout());
       mainPan.add(sendPan, BorderLayout.SOUTH);
       //text to send
-      msgField = new JTextArea(4, 25);
+      msgField = new JTextArea(2, 25);
       scrollText = new JScrollPane(msgField);
       sendPan.add(scrollText, BorderLayout.CENTER);
       //send button
@@ -36,18 +41,19 @@ public class ChatPanel extends JApplet
  
 
       msgPan = new JPanel();
-      msgPan.setLayout(new GridLayout(30, 1));
+      msgPan.setLayout(new GridLayout(numMsgs, 1));
       scrollMsg = new JScrollPane(msgPan);
       mainPan.add(scrollMsg, BorderLayout.CENTER);
       
 
-
-      JPanel pans[] = new JPanel[30];
+      //users[0] = new User(kbyk, );
+      JPanel pans[] = new JPanel[numMsgs];
       
-      for(int i = 0; i < 30; i++)
+      for(int i = 0; i < numMsgs; i++)
       {
         pans[i] = new JPanel();
         pans[i].add(new JLabel("<html>testing " + i + " asldk<br>jfwoieslksldfwoiefj<br><br>dslkcnaliweraofsdvnwir92834urwe<br>fsdjfhasdfhsadjfkasjhdfas<br><br><br></html>"));
+        //pans[i].add(new JLabel("<html>users[0]<br>"blah blah blah blah"</html>);
         msgPan.add(pans[i]); 
       }
       
