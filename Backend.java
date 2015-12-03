bd6716c2ba6e5265e3a86c76fbd5d24d3eeb6a92
Stephen Selke <stephen.selke@gmail.com>
2015-12-03 11:03:46 -0600

    tabs with spaces

--- Backend.java
+++ Backend.java
@@ -15,19 +15,19 @@ import java.util.Enumeration;
 
 public class Backend 
 {
-	private final int PUBLIC_PORT = 7007;                 //port to connect to
-	private final String PROTOCOL_WHOIS = "WhoIs";        //who is string
-	private final String PROTOCOL_FROM = "MessageFrom";   //from string
-	private final String PROTOCOL_RECIEVED = "GotIt";     //received string
-	private User me;                                      //current User
-	private ArrayList<User> users;                        //list of all people current User can chat with
-	private Thread incomingListener;                      //thread for listening
+   private final int PUBLIC_PORT = 7007;                 //port to connect to
+   private final String PROTOCOL_WHOIS = "WhoIs";        //who is string
+   private final String PROTOCOL_FROM = "MessageFrom";   //from string
+   private final String PROTOCOL_RECIEVED = "GotIt";     //received string
+   private User me;                                      //current User
+   private ArrayList<User> users;                        //list of all people current User can chat with
+   private Thread incomingListener;                      //thread for listening
    private String myIP;                                  //current User's IP address
    private String myUsername;                            //current User's username
    private UIWindow window;                              //front end connection
 
 
-	public Backend(UIWindow window) 
+   public Backend(UIWindow window) 
    //PRE:  window is initialized
    //POST: constructs an instance of the backend class 
    { 
@@ -93,38 +93,38 @@ public class Backend
    }
 
 
-	public ArrayList<User> getUsers() 
+   public ArrayList<User> getUsers() 
     //POST: FCTVAL == users
    {
-		return users;
-	}
+      return users;
+   }
 
-	private class MiniScan extends Thread 
+   private class MiniScan extends Thread 
    {
-	   //private int id;      //
+      //private int id;      //
       private int min;     // minimum of scan range
-		private int max;     // max of scan range
+      private int max;     // max of scan range
 
-		public MiniScan(int min, int max) 
+      public MiniScan(int min, int max) 
         //PRE: min and max are valid 0-255
         //POST: Scans the network
         {
-			super();
+         super();
 
-			this.min = min;
-			this.max = max;
-			start();
-		}
+         this.min = min;
+         this.max = max;
+         start();
+      }
 
-		@Override
-		public void run() 
+      @Override
+      public void run() 
       //POST: Runs scanRange function
       {
-			scanRange(min, max);
-		}
-	}
+         scanRange(min, max);
+      }
+   }
 
-	public void scanRange(int min, int max) 
+   public void scanRange(int min, int max) 
    //PRE: min and max are valid 0-255
    //POST: Will scan IPs: prefix+range(min, max)
    {
@@ -132,42 +132,42 @@ public class Backend
       String[] myIPArr;  // current User's IP split into chunks
       String prefix;     // the prefix of the current ip
       
-		myIPArr = me.getHost().split("\\.");
-		prefix = myIPArr[0] + "." + myIPArr[1] + "." + myIPArr[2] + ".";
-		for(int subAddress = min; subAddress <= max; subAddress++) 
+      myIPArr = me.getHost().split("\\.");
+      prefix = myIPArr[0] + "." + myIPArr[1] + "." + myIPArr[2] + ".";
+      for(int subAddress = min; subAddress <= max; subAddress++) 
       {
-			if(!me.getHost().equals(prefix + subAddress)) // don't add yourself
+         if(!me.getHost().equals(prefix + subAddress)) // don't add yourself
          { 
-				u = handshake(prefix + subAddress);
-		
-      		if(u != null)                             // if u isn't null
+            u = handshake(prefix + subAddress);
+      
+            if(u != null)                             // if u isn't null
             {
-					addUser(u);
-				}
-			}
-		}
-	}
+               addUser(u);
+            }
+         }
+      }
+   }
 
-	private void addUser(User u) 
+   private void addUser(User u) 
    //PRE: user is initialized
    //POST: A new user will be added if it does not already exist
    {
-		for(User user : users) 
+      for(User user : users) 
       {
-			if(user.getHost().equals(u.getHost())) 
+         if(user.getHost().equals(u.getHost())) 
          { 
-         	if(!user.getUsername().equals(u.getUsername())) //only re-add if they changed their username
-         	{
+            if(!user.getUsername().equals(u.getUsername())) //only re-add if they changed their username
+            {
                users.remove(user);
                users.add(u);
-         	}
-				return;
-			}
-		}
-		users.add(u);
-	}
+            }
+            return;
+         }
+      }
+      users.add(u);
+   }
 
-	public User addStaticUser(String host) 
+   public User addStaticUser(String host) 
    //PRE: host is a valid IP address
    //POST: FCTVAL == u; the User you are connecting with
    {
@@ -182,7 +182,7 @@ public class Backend
    }
 
 
-	public void scanLAN(int split) 
+   public void scanLAN(int split) 
    //PRE: split is the thread count
    //POST: scan local area network, with split # of threads
    {
@@ -197,7 +197,7 @@ public class Backend
    }
 
 
-	private User handshake(String host) 
+   private User handshake(String host) 
    //PRE: host is a valid IP address
    //POST: Try to make a connection with host, returns new user instance from host
    {
@@ -223,20 +223,20 @@ public class Backend
          }
 
          out.close();
-			in.close();
-			connection.close();
-			return new User(username, host, this);
+         in.close();
+         connection.close();
+         return new User(username, host, this);
       }
-		catch(Exception e) {}
-		return null;
-	}
+      catch(Exception e) {}
+      return null;
+   }
 
-	private void setupThread() 
+   private void setupThread() 
    //POST: a new listening thread will be created
    {
       incomingListener = new Thread() 
       {
-   		public void run() 
+         public void run() 
          //POST: thread will run
          {
             ServerSocket openSocket;   // the server socket for listening
@@ -244,96 +244,96 @@ public class Backend
             InputStream in;            // the input stream of the connection
             OutputStream out;          // the output stream of the connection
          
-				openSocket = null;         // try to connect
-				try 
+            openSocket = null;         // try to connect
+            try 
             {
-					openSocket = new ServerSocket(PUBLIC_PORT);
-				}
-	
-   			catch(Exception e)         // we couldn't listen on the given port
+               openSocket = new ServerSocket(PUBLIC_PORT);
+            }
+   
+            catch(Exception e)         // we couldn't listen on the given port
             {
-					System.err.println("Cannot bind to port %d".format(""+PUBLIC_PORT));
-					System.exit(0);
-				}
+               System.err.println("Cannot bind to port %d".format(""+PUBLIC_PORT));
+               System.exit(0);
+            }
 
-				while(true)               // main loop; loops until window is closed
+            while(true)               // main loop; loops until window is closed
             {
-				   try 
+               try 
                {
-				   	connection = openSocket.accept(); // blocking accept function, waiting for 
-						in = connection.getInputStream();
-						out = connection.getOutputStream();
-						handelIncomingRequest(in, out);
-						in.close();
-						out.close();
-						connection.close();
-					}
-					catch(Exception e) {}
-				}
-			}
-		};
-	}
+                  connection = openSocket.accept(); // blocking accept function, waiting for 
+                  in = connection.getInputStream();
+                  out = connection.getOutputStream();
+                  handelIncomingRequest(in, out);
+                  in.close();
+                  out.close();
+                  connection.close();
+               }
+               catch(Exception e) {}
+            }
+         }
+      };
+   }
 
 
-	private void handelIncomingRequest(InputStream in, OutputStream out) 
+   private void handelIncomingRequest(InputStream in, OutputStream out) 
    //PRE: in is valid, out is valid
    //POST: We will handle the request properly
    {
       String header = "";     // header of the network packet
-		String message = "";    // message sent in the network packet
-		char c;                 // used to parse the header
+      String message = "";    // message sent in the network packet
+      char c;                 // used to parse the header
 
-		try 
+      try 
       {
-			while((c = (char) in.read()) != 0x0)               // read in header
+         while((c = (char) in.read()) != 0x0)               // read in header
          {
-		   	header += c;
-			}
+            header += c;
+         }
          
-			if(header.equals(PROTOCOL_WHOIS))                  // if it is the WHOIS protocol
+         if(header.equals(PROTOCOL_WHOIS))                  // if it is the WHOIS protocol
          {
-				out.write(me.getUsername().getBytes());
-				out.write(0x0);
-				out.flush();
-			}
+            out.write(me.getUsername().getBytes());
+            out.write(0x0);
+            out.flush();
+         }
          
-			else if(header.indexOf(PROTOCOL_FROM + ":") != -1) // else if the from protocol is >= 0
+         else if(header.indexOf(PROTOCOL_FROM + ":") != -1) // else if the from protocol is >= 0
          {
-				String[] parse;                          
+            String[] parse;                          
             
             parse = header.split(":");
-				while((c = (char) in.read()) != 0x0)            // read message 
+            while((c = (char) in.read()) != 0x0)            // read message 
             {
-					message += c;
-				}
+               message += c;
+            }
             
-				messageNotify(parse[1], message);
-				out.write(PROTOCOL_RECIEVED.getBytes());
-				out.write(0x0);
-				out.flush();
-			}
-		}
-		catch(Exception e) {}
-	}
+            messageNotify(parse[1], message);
+            out.write(PROTOCOL_RECIEVED.getBytes());
+            out.write(0x0);
+            out.flush();
+         }
+      }
+      catch(Exception e) {}
+   }
 
 
-	private void messageNotify(String username, String message) 
+   private void messageNotify(String username, String message) 
    //PRE:  username and message is valid
    //POST: the user's ChatPanel will be updated
    {
       for(User u : users) 
       {
-        	if(u.getUsername().equals(username))                  // if User's username == username
+           if(u.getUsername().equals(username))                  // if User's username == username
          {         
-      		u.getChatPanel().msgReceived(message, username);   // call msgReceived in ChatPanel to
-      		window.changeToUser(u);                       
-      		return;                                            // display the message in ChatPanel
-      	}
+            u.getChatPanel().msgReceived(message, username);   // call msgReceived in ChatPanel to
+            window.changeToUser(u);                       
+            return;                                            // display the message in ChatPanel
+         }
       }
-	}
+   }
 
 
-	public boolean sendMessage(User user, String message) 
+   public boolean sendMessage(User user, String message) 
    //PRE: user is valid
    //POST: the message will be sent
    {
@@ -344,38 +344,38 @@ public class Backend
       InputStream in;         // input stream of bytes
       char c;                 // used for parsing the header
       
-		try                                       // try to send the message
+      try                                       // try to send the message
       {
-		   connection = new Socket(user.getHost(), PUBLIC_PORT);
-			out = connection.getOutputStream();
-			in = connection.getInputStream();
+         connection = new Socket(user.getHost(), PUBLIC_PORT);
+         out = connection.getOutputStream();
+         in = connection.getInputStream();
 
-			m = PROTOCOL_FROM + ":" + me.getUsername();
-			out.write(m.getBytes());
-			out.write(0x0);
-			out.write(message.getBytes());
-			out.write(0x0);
-			out.flush();
-			header = "";
+         m = PROTOCOL_FROM + ":" + me.getUsername();
+         out.write(m.getBytes());
+         out.write(0x0);
+         out.write(message.getBytes());
+         out.write(0x0);
+         out.flush();
+         header = "";
 
-			while((c = (char) in.read()) != 0x0)   // read in header
+         while((c = (char) in.read()) != 0x0)   // read in header
          {
-				header += c;
-			}
+            header += c;
+         }
          
-			if(header.equals(PROTOCOL_RECIEVED))   // if header == "GotIt", return true
+         if(header.equals(PROTOCOL_RECIEVED))   // if header == "GotIt", return true
          {
-				return true;
-			}
+            return true;
+         }
          
-			out.close();
-			in.close();
+         out.close();
+         in.close();
          connection.close();
-		}
+      }
       
-		catch(Exception e) {}
-		return false;                            // else return false
-	}
+      catch(Exception e) {}
+      return false;                            // else return false
+   }
    
    
    public String getMyIP()
